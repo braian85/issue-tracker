8c6d28dd19193899226f67e8ca0b83ba5355645f
Braian Savoretti <braian@gmail.com>
2024-10-28 15:47:35 -0300

    refresh token

--- src/api/auth/[...nextauth].ts
+++ src/api/auth/[...nextauth].ts
@@ -7,16 +7,19 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
-    // ...add more providers here
   ],
   callbacks: {
     async signIn({ user, account }) {
-      console.log('está en signIn')
-      console.log({ user, account })
       if (account && account.provider === 'google') {
         try {
-          // Aquí es donde guardamos el usuario en el backend
           const response = await axios.post(
             `${process.env.BACKEND_URL}/api/users`,
             {
@@ -43,24 +46,63 @@ export const authOptions: NextAuthOptions = {
       return true
     },
     async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      console.log('está en jwt')
-      console.log({ token, account })
-      if (account && 'access_token' in account) {
+      // Initial sign in
+      if (account) {
         token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+        token.expiresAt = account.expires_at! * 1000 // Convert to milliseconds
       }
-      return token
+
+      // Return previous token if the access token has not expired
+      if (Date.now() < token.expiresAt) {
+        return token
+      }
+
+      // Access token has expired, refresh it
+      return await refreshAccessToken(token)
     },
     async session({ session, token }) {
-      console.log('está en session')
-      console.log({ session, token })
-      // Send properties to the client, like an access_token from a provider.
-      if ('accessToken' in token) {
-        session.accessToken = token.accessToken as string
-      }
+      session.accessToken = token.accessToken
       return session
     },
   },
+}
+
+async function refreshAccessToken(token) {
+  try {
+    const url =
+      "https://oauth2.googleapis.com/token?" +
+      new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID!,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken,
+      })
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      expiresAt: Date.now() + refreshedTokens.expires_in * 1000,
+    }
+  } catch (error) {
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
 }
 
 export default NextAuth(authOptions)
