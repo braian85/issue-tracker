de16f96ff93ef4fdd25c4451dba72c77c62d3620
Braian Savoretti <braian@gmail.com>
2024-12-06 19:04:48 -0300

    style improvements WIP

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -8,34 +8,25 @@ export default function Sidebar() {
   }
 
   return (
-    <aside className='w-64 bg-background shadow-md h-screen overflow-y-auto flex flex-col'>
+    <aside className='w-64 bg-white dark:bg-gray-900 border-r-2 border-gray-300 dark:border-gray-700 shadow-lg h-screen overflow-y-auto flex flex-col'>
       <div className='p-4 flex-shrink-0'>
         <IssueTrackerLogo />
       </div>
       <nav className='mt-8 flex-grow'>
-        <Link
-          href='/dashboard'
-          className='block py-2 px-4 text-foreground border border-transparent hover:border hover:border-violet-700 transition-all duration-500'
-        >
+        <Link href='/dashboard' className='block py-2 px-4 text-blue-900 dark:text-blue-300 hover:bg-blue-300 dark:hover:bg-gray-800'>
           Dashboard
         </Link>
-        <Link
-          href='/projects'
-          className='block py-2 px-4 text-foreground border border-transparent hover:border hover:border-violet-700 transition-all duration-500'
-        >
+        <Link href='/projects' className='block py-2 px-4 text-blue-900 dark:text-blue-300 hover:bg-blue-300 dark:hover:bg-gray-800'>
           Projects
         </Link>
-        <Link
-          href='/settings'
-          className='block py-2 px-4 text-foreground border border-transparent hover:border hover:border-violet-700 transition-all duration-500'
-        >
+        <Link href='/settings' className='block py-2 px-4 text-blue-900 dark:text-blue-300 hover:bg-blue-300 dark:hover:bg-gray-800'>
           Settings
         </Link>
       </nav>
       <div className='p-4'>
         <button
           onClick={handleLogout}
-          className='w-full py-2 px-4 text-foreground bg-background hover:bg-accent rounded'
+          className='w-full py-2 px-4 text-white bg-blue-600 dark:bg-blue-800 hover:bg-blue-700 dark:hover:bg-blue-900 rounded'
         >
           Logout
         </button>
